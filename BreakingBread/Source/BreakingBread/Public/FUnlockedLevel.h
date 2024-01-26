bc2811057b9998c9daf805d35920abe1c58aa7ae
Xander Berten <berten.xander@gmail.com>
2024-01-27 00:42:51 +0100

    Added ProperLevel Changing Based on EventTypes

---
+++ BreakingBread/Source/BreakingBread/Public/FUnlockedLevel.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Engine/DataTable.h"
+#include "FUnlockedLevel.generated.h"
+
+UENUM(BlueprintType)
+enum class EUnlockEvents : uint8
+{
+	VE_BroodInRooster      UMETA(DisplayName="Broos in Rooster"),
+	VE_Test2      UMETA(DisplayName="Test2"),
+};
+
+
+
+USTRUCT(BlueprintType) struct FUnlockedLevel : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	FUnlockedLevel() : Level(nullptr), UnlockedByKey(EUnlockEvents::VE_BroodInRooster) 
+	{	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UWorld> Level;
+	
+	//Added a unlocked by key that will be send by events
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EUnlockEvents UnlockedByKey;
+};
---
+++ BreakingBread/Source/BreakingBread/Public/WorldTools.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WorldTools.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREAKINGBREAD_API UWorldTools : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	public:
+	UFUNCTION(BlueprintCallable, Category = "WorldTools")
+	static FName GetLevelName(TSoftObjectPtr<UWorld> World);
+};
