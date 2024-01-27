d60290f3cbd68d09bf55c4c6013a6bbec7bada66
Xander Berten <berten.xander@gmail.com>
2024-01-27 16:19:26 +0100

    Added generic Ending in the struct

--- BreakingBread/Source/BreakingBread/Public/FUnlockedLevel.h
+++ BreakingBread/Source/BreakingBread/Public/FUnlockedLevel.h
@@ -5,8 +5,21 @@
 UENUM(BlueprintType)
 enum class EUnlockEvents : uint8
 {
-	VE_NormalEnding UMETA(DisplayName="Normal Ending"),
-	VE_Test2       UMETA(DisplayName="Test2"),
+	VE_NormalEnding					UMETA(DisplayName="Normal Ending"),
+	VE_PlateEnding					UMETA(DisplayName="Dry Ending"),
+	VE_ToasterEnding 				UMETA(DisplayName="Toaster Ending: Toasty!"),
+	VE_SinkEnding					UMETA(DisplayName="Sink Ending: Let that sink in!"),
+	VE_FridgeEnding					UMETA(DisplayName="Fridge Ending: Cold as ice!"),
+	VE_OvenEnding					UMETA(DisplayName="Oven Ending: Hot as hell!"),
+	VE_MicrowaveEnding				UMETA(DisplayName="Microwave Ending: Ding!"),
+	VE_DishwasherEnding				UMETA(DisplayName="Dishwasher Ending: Clean as a whistle!"),
+	VE_CupboardEnding				UMETA(DisplayName="Cupboard Ending: Out of sight, out of mind!"),
+	VE_CounterEnding				UMETA(DisplayName="Counter Ending: A place for everything and everything in its place!"),
+	VE_CuttingBoardEnding			UMETA(DisplayName="Cutting Board Ending: Chop chop!"),
+	VE_BowlingEnding				UMETA(DisplayName="Bowling Ending: Strike!"),
+	VE_KatapultEnding				UMETA(DisplayName="Katapult Ending: Yeet!"),
+	VE_RocketEnding					UMETA(DisplayName="Rocket Ending: Blast off!"),
+	VE_JesusEnding					UMETA(DisplayName="Jesus Ending: Holy moly!"),
 };
 
 
